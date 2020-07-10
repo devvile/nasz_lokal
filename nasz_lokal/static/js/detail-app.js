3f8ed2eb230d9e457f759a9a9cfe0f8d19e3eca1
Dottore <patryk.czemierowski@gmail.com>
2020-07-10 17:57:34 +0200

    dziedziczenie koloru przez pole tekstu, filtry na wpisach parzyscie

--- nasz_lokal/static/js/detail-app.js
+++ nasz_lokal/static/js/detail-app.js
@@ -2,17 +2,30 @@
 const cats = document.querySelectorAll("div.func");
 const dogs = document.querySelectorAll("div.pole-opisu");
 let storage = localStorage.getItem("elem");
+const poleOpisu = document.querySelectorAll(".pole-opisu");
 
 let i = 0;
 
-// Pole-text ACTIVATION
-cats.forEach((element) => {
-  element.id = i;
-  i++;
-  element.style.backgroundColor = col[storage];
-  element.addEventListener("click", () => {
-    id = element.id;
-    const yourDog = dogs[id];
-    yourDog.classList.toggle("active");
+function main() {
+  // Pole-text ACTIVATION
+  cats.forEach((element) => {
+    element.id = i;
+    i++;
+    element.style.backgroundColor = col[storage];
+    if (element.id % 2 === 0) {
+      element.style.filter = "sepia(30%)";
+    } else {
+      element.style.filter = "sepia(65%)";
+    }
+    element.addEventListener("click", () => {
+      id = element.id;
+      const yourDog = dogs[id];
+      yourDog.classList.toggle("active");
+    });
   });
-});
+  // Pole opisu kolor
+  poleOpisu.forEach((element) => {
+    element.style.backgroundColor = col[storage];
+  });
+}
+main();
