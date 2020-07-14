043e56404c69ee79e1cd81e5cda257829714b51b
Dottore <patryk.czemierowski@gmail.com>
2020-07-14 21:46:20 +0200

    allowed_hosts pod deployment, debug=False

--- nasz_lokal/nasz_lokal/settings.py
+++ nasz_lokal/nasz_lokal/settings.py
@@ -19,13 +19,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '@cs7saq2#-7%5ngso-y8qo0#l6^1s02!iwu-tz9131=k-z*-m*'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '@cs7saq2#-7%5ngso-y8qo0#l6^1s02!iwu-tz9131=k-z*-m*')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+DEBUG = False
+
+ALLOWED_HOSTS = ['devvile.pythonanywhere.com']
 
 
 # Application definition
