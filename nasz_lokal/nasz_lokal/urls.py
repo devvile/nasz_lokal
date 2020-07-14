97c8b33a6e444a01f06033db674a69cf4eb05a5b
Dottore <patryk.czemierowski@gmail.com>
2020-07-14 19:49:43 +0200

    przekierowanie ze strony glownej

--- nasz_lokal/nasz_lokal/urls.py
+++ nasz_lokal/nasz_lokal/urls.py
@@ -1,8 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from .views import red
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cat/', include('notki.urls')),
+    path('',red)
 ]
